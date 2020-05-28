12c09685c4d88b8ee3847a077312192cd0bd868e
Mike Wong <jwong3398@gmail.com>
2020-05-28 16:04:27 -0400

    First Commit/Added index File

---
+++ index.py
@@ -0,0 +1,6 @@
+import pync
+
+# pync.notify('Hello World')
+# pync.notify('Hello World', appIcon='./twitchIcon.png')
+# pync.notify('Hello World', activate='com.apple.Safari')
+pync.notify('Hello World', execute='say "OMG"', appIcon='./twitchIcon.png')
